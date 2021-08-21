e7889503fd31f5a9257f3262d1769a6f217bf558
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-20 19:02:13 -0500

    Building a primary list of items

---
+++ SnowSeeker/Resort.swift
@@ -0,0 +1,20 @@
+//
+//  Resort.swift
+//  SnowSeeker
+//
+//  Created by Antonio Vega on 8/20/21.
+//
+
+struct Resort: Codable, Identifiable {
+    let id: String
+    let name: String
+    let country: String
+    let description: String
+    let imageCredit: String
+    let price: Int
+    let size: Int
+    let snowDepth: Int
+    let elevation: Int
+    let runs: Int
+    let facilities: [String]
+}
