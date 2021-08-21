2ab02dac08f786e892986f845c0b42316eb9a195
antonivegaochoa <tonivegaochoa@gmail.com>
2021-08-21 09:56:41 -0500

    Binding an alert to an optional string

--- SnowSeeker/Resort.swift
+++ SnowSeeker/Resort.swift
@@ -9,6 +9,10 @@ import Foundation
 struct Resort: Codable, Identifiable {
     static let allResorts: [Resort] = Bundle.main.decode("resorts.json")
     static let example = allResorts[0]
+
+    var facilityTypes: [Facility] {
+        facilities.map(Facility.init)
+    }
     
     let id: String
     let name: String
